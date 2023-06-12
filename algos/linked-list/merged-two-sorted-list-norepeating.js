ab5cc79d1fbcf508bb06fe746cadc21b39ea6374
taylrzhang <neil0725@hotmail.com>
2023-06-11 21:26:20 -0700

    merge two sorted list with/without multiples updated

--- algos/linked-list/merged-two-sorted-list-norepeating.js
+++ algos/linked-list/merged-two-sorted-list-norepeating.js
@@ -1,14 +1,11 @@
 /**
+ * Merge two sorted linked lists without repeating
+
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
- * } 
-You are given the heads of two sorted linked lists list1 and list2.
-
-Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
-
-Return the head of the merged linked list.
+ * }
  */
 /**
  * @param {ListNode} list1
@@ -16,29 +13,31 @@ Return the head of the merged linked list.
  * @return {ListNode}
  */
 
-const mergeTwoLists = (list1, list2) => {
+function mergeWithoutReapeating(list1, list2) {
   let head = new ListNode();
-  let cur = head;
+  let curr = head;
 
   while (list1 && list2) {
-    if (list1.val <= list2.val) {
-      cur.next = list1;
+    if (list1.val === list2.val) {
+      curr.next = list1;
+      list1 = list1.next;
+      list2 = list2.next;
+      curr = curr.next;
+      continue;
+    }
+    if (list1.val < list2.val) {
+      curr.next = list1;
       list1 = list1.next;
     } else {
-      cur.next = list2;
+      curr.next = list2;
       list2 = list2.next;
     }
-    cur = cur.next;
+    curr = curr.next;
   }
-  // if(list1 === null) {
-  //   curr.next = list2
-  // }
-  // if(list2 === null) {
-  //   curr.next = list2
-  // }
-  cur.next = list1 === null ? list2 : list1;
+
+  curr.next = list1 === null ? list2 : list1;
   return head.next;
-};
+}
 
 function ListNode(val, next) {
   this.val = val === undefined ? 0 : val;
@@ -53,5 +52,5 @@ const l2 = new ListNode(1);
 l2.next = new ListNode(3);
 l2.next.next = new ListNode(4);
 
-let r1 = mergeTwoLists(l1, l2);
+let r1 = mergeWithoutReapeating(l1, l2);
 console.log(r1);
